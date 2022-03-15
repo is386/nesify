984f9dcf17e3d4f09fb36a7d2d6edd904a82138f
is386 <20195347+is386@users.noreply.github.com>
2022-03-15 14:07:37 -0400

    add basic structure for cpu and mmu

---
+++ emu/nes.go
@@ -0,0 +1,68 @@
+package emu
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/is386/NESify/emu/cpu"
+	"github.com/is386/NESify/emu/mem"
+)
+
+const (
+	CLOCK_SPEED = 1789773
+	FPS         = 60
+	FRAMETIME   = time.Second / time.Duration(FPS)
+	CPS         = CLOCK_SPEED / FPS
+)
+
+type NES struct {
+	cpu     *cpu.CPU
+	mmu     *mem.MMU
+	cyc     int
+	running bool
+}
+
+func NewNES(romFileName string) *NES {
+	nes := &NES{}
+	mmu := mem.NewMMU()
+	nes.mmu = mmu
+	nes.cpu = cpu.NewCPU(mmu)
+	nes.loadRom(romFileName)
+	return nes
+}
+
+func (nes *NES) Run() {
+	ticker := time.NewTicker(FRAMETIME)
+	nes.running = true
+
+	for range ticker.C {
+		if !nes.running {
+			nes.shutdown()
+		}
+		nes.update()
+	}
+}
+
+func (nes *NES) loadRom(romFileName string) {
+	rom, err := ioutil.ReadFile(romFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	for i := range rom {
+		nes.mmu.Write(uint16(i), rom[i])
+	}
+}
+
+func (nes *NES) update() {
+	for nes.cyc < CPS {
+		nes.cyc += nes.cpu.Update()
+	}
+	nes.cyc -= CPS
+}
+
+func (nes *NES) shutdown() {
+	nes.running = false
+}
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/is386/NESify/emu"
+	"github.com/sqweek/dialog"
+)
 
 func main() {
-	fmt.Println("hello world")
+	romFileName, err := dialog.File().Filter("NES Rom File", "nes").Load()
+	if err != nil {
+		panic(err)
+	}
+	n := emu.NewNES(romFileName)
+	n.Run()
 }
