e60075024bc19d9a48ef3da8187fdae4459da7ea
is386 <20195347+is386@users.noreply.github.com>
2022-03-18 16:18:42 -0400

    add first mapper

---
+++ emu/mem/cart.go
@@ -0,0 +1,19 @@
+package mem
+
+type Cart struct {
+	mapper Mapper
+}
+
+func newCart(rom []uint8) Cart {
+	c := Cart{mapper: newNROM()}
+	c.mapper.loadRom(rom)
+	return c
+}
+
+func (c *Cart) read(addr uint16) uint8 {
+	return c.mapper.read(addr)
+}
+
+func (c *Cart) write(addr uint16, val uint8) {
+	c.mapper.write(addr, val)
+}
--- emu/mem/mmu.go
+++ emu/mem/mmu.go
@@ -1,17 +1,58 @@
 package mem
 
+import "fmt"
+
 type MMU struct {
-	ram [0xFFFF]uint8
+	ram  [0x800]uint8
+	cart Cart
 }
 
-func NewMMU() *MMU {
-	return &MMU{}
+func NewMMU(rom []uint8) *MMU {
+	return &MMU{cart: newCart(rom)}
 }
 
 func (mmu *MMU) Read(addr uint16) uint8 {
-	return mmu.ram[addr]
+	switch {
+
+	case addr < 0x2000:
+		return mmu.ram[addr%0x800]
+
+	case addr < 0x4000 || addr == 0x4014:
+		fmt.Printf("reading PPU register at: %X\n", addr)
+		return 0
+
+	case addr < 0x4018:
+		fmt.Printf("reading APU/IO register at: %X\n", addr)
+		return 0
+
+	case addr < 0x4020:
+		fmt.Printf("reading disabled APU/IO register at: %X\n", addr)
+		return 0
+
+	case addr < 0xFFFF:
+		return mmu.cart.read(addr)
+
+	default:
+		return 0
+	}
 }
 
 func (mmu *MMU) Write(addr uint16, val uint8) {
-	mmu.ram[addr] = val
+	switch {
+
+	case addr < 0x2000:
+		mmu.ram[addr%0x800] = val
+
+	case addr < 0x4000 || addr == 0x4014:
+		fmt.Printf("writing PPU register at: %X\n", addr)
+
+	case addr < 0x4018:
+		fmt.Printf("writing APU/IO register at: %X\n", addr)
+
+	case addr < 0x4020:
+		fmt.Printf("writing disabled APU/IO register at: %X\n", addr)
+
+	case addr < 0xFFFF:
+		mmu.cart.write(addr, val)
+	}
 }
