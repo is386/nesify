6bc72e58a883208fa35176c683ccf7eea83164de
is386 <20195347+is386@users.noreply.github.com>
2022-03-22 21:29:35 -0400

    add colors

--- emu/ppu.go
+++ emu/ppu.go
@@ -22,7 +22,16 @@ const (
 )
 
 var (
-	COLORS = []uint32{0x000000, 0x545454, 0xA9A9A9, 0xEFEFEF}
+	COLORS = []uint32{
+		0x666666, 0x002A88, 0x1412A7, 0x3B00A4, 0x5C007E, 0x6E0040, 0x6C0600, 0x561D00,
+		0x333500, 0x0B4800, 0x005200, 0x004F08, 0x00404D, 0x000000, 0x000000, 0x000000,
+		0xADADAD, 0x155FD9, 0x4240FF, 0x7527FE, 0xA01ACC, 0xB71E7B, 0xB53120, 0x994E00,
+		0x6B6D00, 0x388700, 0x0C9300, 0x008F32, 0x007C8D, 0x000000, 0x000000, 0x000000,
+		0xFFFEFF, 0x64B0FF, 0x9290FF, 0xC676FF, 0xF36AFF, 0xFE6ECC, 0xFE8170, 0xEA9E22,
+		0xBCBE00, 0x88D800, 0x5CE430, 0x45E082, 0x48CDDE, 0x4F4F4F, 0x000000, 0x000000,
+		0xFFFEFF, 0xC0DFFF, 0xD3D2FF, 0xE8C8FF, 0xFBC2FF, 0xFEC4EA, 0xFECCC5, 0xF7D8A5,
+		0xE4E594, 0xCFEF96, 0xBDF4AB, 0xB3F3CC, 0xB5EBF2, 0xB8B8B8, 0x000000, 0x000000,
+	}
 )
 
 type PPU struct {
@@ -37,9 +46,10 @@ type PPU struct {
 
 func NewPPU(b *PpuBus) *PPU {
 	p := &PPU{bus: b}
-	p.screen = NewScreen(NES_WIDTH+CHR_WIDTH, CHR_HEIGHT, SCALE)
+	//p.screen = NewScreen(NES_WIDTH+CHR_WIDTH, CHR_HEIGHT, SCALE)
+	p.screen = NewScreen(NES_WIDTH, NES_HEIGHT, SCALE)
 	p.screen.win.SetTitle("NESify")
-	p.showCHR()
+	//p.showCHR()
 	return p
 }
 
@@ -68,26 +78,37 @@ func (p *PPU) renderNameTables() {
 	ntAddr := p.getNameTableAddr()
 	baseX := 0
 	for ntByte := uint16(0); ntByte < 960; ntByte++ {
-		ptIdx := p.bus.read(ntAddr)
+		ptIdx := p.bus.read(ntAddr + ntByte)
 		ptBaseAddr := p.getBgPatternTableAddr() + (uint16(ptIdx) * 16)
 
-		y := 0
 		for ptAddr := ptBaseAddr; ptAddr < ptBaseAddr+8; ptAddr++ {
 			ptByte1 := p.bus.read(ptAddr)
 			ptByte2 := p.bus.read(ptAddr + 8)
+			y := int(ptAddr-ptBaseAddr) + int(ntByte/32)*8
 
 			for x := baseX; x < baseX+8; x++ {
 				pixel := 7 - (x % 8)
 				colorBit0 := (ptByte1 >> pixel) & 1
 				colorBit1 := (ptByte2 >> pixel) & 1
 				colorNum := (colorBit1 << 1) | colorBit0
-				p.screen.drawPixel(int32(x), int32(y+int(ntByte/32)*8), COLORS[colorNum])
-			}
-			y++
-		}
 
+				blockX := x / 32
+				blockY := y / 32
+				blockAddr := uint16(8*blockY) + uint16(blockX) + ntAddr + 0x3C0
+				blockByte := p.bus.read(blockAddr)
+
+				quadX := x / 16
+				quadY := y / 16
+				quad := uint8(((quadY % 2) << 1) | (quadX % 2))
+
+				paletteBit0 := bits.Value(blockByte, (quad * 2))
+				paletteBit1 := bits.Value(blockByte, (quad*2)+1)
+				paletteNum := (paletteBit1 << 1) | paletteBit0
+
+				p.screen.drawPixel(int32(x), int32(y), p.getPalette(int(paletteNum))[colorNum])
+			}
+		}
 		baseX = (baseX + 8) % NES_WIDTH
-		ntAddr++
 	}
 	p.screen.Update()
 }
@@ -102,7 +123,7 @@ func (p *PPU) showCHR() {
 			colorBit0 := (tileByte1 >> pixel) & 1
 			colorBit1 := (tileByte2 >> pixel) & 1
 			colorNum := (colorBit1 << 1) | colorBit0
-			p.screen.drawPixel(int32(x)+NES_WIDTH, int32(y), COLORS[colorNum])
+			p.screen.drawPixel(int32(x)+NES_WIDTH, int32(y), COLORS[colorNum*3])
 		}
 	}
 	p.screen.Update()
@@ -186,4 +207,14 @@ func (p *PPU) setVblank() {
 func (p *PPU) resetVblank() {
 	p.ppuStatus = bits.Reset(p.ppuStatus, 7)
 	p.nmiOccurred = false
+}
+
+func (p *PPU) getPalette(num int) []uint32 {
+	addr := 0x3F00 + (num * 4)
+	var palette []uint32
+	for i := addr; i < addr+4; i++ {
+		paletteByte := p.bus.read(uint16(i))
+		palette = append(palette, COLORS[paletteByte])
+	}
+	return palette
 }
